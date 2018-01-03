a3c2f37b0db68bfbc6a2fd117aee31e4ef8c3c3d
exx <exx@coffee.tch.harvard.edu>
2018-01-03 14:40:03 -0500

    test util functions

--- util.py
+++ util.py
@@ -1,6 +1,19 @@
+import nibabel as nib
 import nilearn.image as nil_image
 import numpy as np
 import SimpleITK as sitk
+
+def load_3d_data(
+    filename,
+):
+    """Loads data.
+    """
+    img = nib.load(filename)
+
+    volume = img.get_data()
+    affine = img.affine
+
+    return volume, affine
 
 
 def create_rotation_matrix(param):
@@ -39,8 +52,8 @@ def create_rotation_matrix(param):
     
     # Apply the rotation first around Y then X then Z.
     # To follow ITK transformation functions.
-    rot = np.matmul(Rz, Rx) 
-    rot = np.matmul(rot, Ry)
+    rot = np.matmul(Rz, Ry) 
+    rot = np.matmul(rot, Rx)
 
     return rot
 
@@ -93,16 +106,17 @@ def create_affine_matrix(
         affine_trans = np.matmul(affine_trans, affine_trans_rot)
         affine_trans = np.matmul(affine_trans, affine_trans_shift)
 
-        return affine_trans
+        return affine_trans, rotation
 
 
 def similarity_transform_volumes(
     image,
     affine_trans,
     target_size,
+    interpolation = 'continuous',
 ):
     image_size = np.shape(image)
-    possible_scales = np.divide(image_size, target_size)
+    possible_scales = np.true_divide(image_size, target_size)
     crop_scale = np.max(possible_scales)
     if crop_scale <= 1:
         crop_scale = 1
@@ -197,3 +211,141 @@ def vrrotvec2mat(ax_ang):
         mtx = mtx.reshape(msz, 3, 3)
 
     return mtx
+
+
+def vrrotmat2vec(mat1, rot_type='proper'):
+    """
+    Create an axis-angle np.array from Rotation Matrix:
+    ====================
+
+    @param mat:  The nx3x3 rotation matrices to convert
+    @type mat:   nx3x3 numpy array
+
+    @param rot_type: 'improper' if there is a possibility of
+                      having improper matrices in the input,
+                      'proper' otherwise. 'proper' by default
+    @type  rot_type: string ('proper' or 'improper')
+
+    @return:    The 3D rotation axis and angle (ax_ang)
+                5 entries:
+                   First 3: axis
+                   4: angle
+                   5: 1 for proper and -1 for improper
+    @rtype:     numpy 5xn array
+
+    """
+    mat = np.copy(mat1)
+    if mat.ndim == 2:
+        if np.shape(mat) == (3, 3):
+            mat = np.copy(np.reshape(mat, (1, 3, 3)))
+        else:
+            raise Exception('Wrong Input Type')
+    elif mat.ndim == 3:
+        if np.shape(mat)[1:] != (3, 3):
+            raise Exception('Wrong Input Type')
+    else:
+        raise Exception('Wrong Input Type')
+
+    msz = np.shape(mat)[0]
+    ax_ang = np.zeros((5, msz))
+
+    epsilon = 1e-12
+    if rot_type == 'proper':
+        ax_ang[4, :] = np.ones(np.shape(ax_ang[4, :]))
+    elif rot_type == 'improper':
+        for i in range(msz):
+            det1 = np.linalg.det(mat[i, :, :])
+            if abs(det1 - 1) < epsilon:
+                ax_ang[4, i] = 1
+            elif abs(det1 + 1) < epsilon:
+                ax_ang[4, i] = -1
+                mat[i, :, :] = -mat[i, :, :]
+            else:
+                raise Exception('Matrix is not a rotation: |det| != 1')
+    else:
+        raise Exception('Wrong Input parameter for rot_type')
+
+
+
+    mtrc = mat[:, 0, 0] + mat[:, 1, 1] + mat[:, 2, 2]
+
+
+    ind1 = np.where(abs(mtrc - 3) <= epsilon)[0]
+    ind1_sz = np.size(ind1)
+    if np.size(ind1) > 0:
+        ax_ang[:4, ind1] = np.tile(np.array([0, 1, 0, 0]), (ind1_sz, 1)).transpose()
+
+
+    ind2 = np.where(abs(mtrc + 1) <= epsilon)[0]
+    ind2_sz = np.size(ind2)
+    if ind2_sz > 0:
+        # phi = pi
+        # This singularity requires elaborate sign ambiguity resolution
+
+        # Compute axis of rotation, make sure all elements >= 0
+        # real signs are obtained by flipping algorithm below
+        diag_elems = np.concatenate((mat[ind2, 0, 0].reshape(ind2_sz, 1),
+                                     mat[ind2, 1, 1].reshape(ind2_sz, 1),
+                                     mat[ind2, 2, 2].reshape(ind2_sz, 1)), axis=1)
+        axis = np.sqrt(np.maximum((diag_elems + 1)/2, np.zeros((ind2_sz, 3))))
+        # axis elements that are <= epsilon are set to zero
+        axis = axis*((axis > epsilon).astype(int))
+
+        # Flipping
+        #
+        # The algorithm uses the elements above diagonal to determine the signs
+        # of rotation axis coordinate in the singular case Phi = pi.
+        # All valid combinations of 0, positive and negative values lead to
+        # 3 different cases:
+        # If (Sum(signs)) >= 0 ... leave all coordinates positive
+        # If (Sum(signs)) == -1 and all values are non-zero
+        #   ... flip the coordinate that is missing in the term that has + sign,
+        #       e.g. if 2AyAz is positive, flip x
+        # If (Sum(signs)) == -1 and 2 values are zero
+        #   ... flip the coord next to the one with non-zero value
+        #   ... ambiguous, we have chosen shift right
+
+        # construct vector [M23 M13 M12] ~ [2AyAz 2AxAz 2AxAy]
+        # (in the order to facilitate flipping):    ^
+        #                                  [no_x  no_y  no_z ]
+
+        m_upper = np.concatenate((mat[ind2, 1, 2].reshape(ind2_sz, 1),
+                                  mat[ind2, 0, 2].reshape(ind2_sz, 1),
+                                  mat[ind2, 0, 1].reshape(ind2_sz, 1)), axis=1)
+
+        # elements with || smaller than epsilon are considered to be zero
+        signs = np.sign(m_upper)*((abs(m_upper) > epsilon).astype(int))
+
+        sum_signs = np.sum(signs, axis=1)
+        t1 = np.zeros(ind2_sz,)
+        tind1 = np.where(sum_signs >= 0)[0]
+        t1[tind1] = np.ones(np.shape(tind1))
+
+        tind2 = np.where(np.all(np.vstack(((np.any(signs == 0, axis=1) == False), t1 == 0)), axis=0))[0]
+        t1[tind2] = 2*np.ones(np.shape(tind2))
+
+        tind3 = np.where(t1 == 0)[0]
+        flip = np.zeros((ind2_sz, 3))
+        flip[tind1, :] = np.ones((np.shape(tind1)[0], 3))
+        flip[tind2, :] = np.copy(-signs[tind2, :])
+
+        t2 = np.copy(signs[tind3, :])
+
+        shifted = np.column_stack((t2[:, 2], t2[:, 0], t2[:, 1]))
+        flip[tind3, :] = np.copy(shifted + (shifted == 0).astype(int))
+
+        axis = axis*flip
+        ax_ang[:4, ind2] = np.vstack((axis.transpose(), np.pi*(np.ones((1, ind2_sz)))))
+
+    ind3 = np.where(np.all(np.vstack((abs(mtrc + 1) > epsilon, abs(mtrc - 3) > epsilon)), axis=0))[0]
+    ind3_sz = np.size(ind3)
+    if ind3_sz > 0:
+        phi = np.arccos((mtrc[ind3]-1)/2)
+        den = 2*np.sin(phi)
+        a1 = (mat[ind3, 2, 1]-mat[ind3, 1, 2])/den
+        a2 = (mat[ind3, 0, 2]-mat[ind3, 2, 0])/den
+        a3 = (mat[ind3, 1, 0]-mat[ind3, 0, 1])/den
+        axis = np.column_stack((a1, a2, a3))
+        ax_ang[:4, ind3] = np.vstack((axis.transpose(), phi.transpose()))
+
+    return ax_ang
