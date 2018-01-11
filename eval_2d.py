fix rotation distance bug

eval_2d.py
@@ -17,7 +17,7 @@ from .util import (
     vrrotmat2vec,
     geodesic_distance,
     auto_crop,
-    
+    rot_distance,
 )
 
 # -----------------------------------------------------------------------------
@@ -27,7 +27,7 @@ depth = 120
 n_channels = 1 
 
 number_of_eval = 100
-model_path = '/host/home/exx/Documents/resources/model/2dTo3d_2.h5'
+model_path = '/host/home/exx/Documents/resources/model/3view_2dTo3d_4.h5'
 data_path = './DeepRegistration/SampleData/' 
 # -----------------------------------------------------------------------------
 
@@ -54,7 +54,7 @@ def eval_model(
 
     croped_image = auto_crop(mov_img)
     
-    for i in range(number_of_eval):
+    for i in range(1,4):
 
         affine_mov, rotation_mov = create_affine_matrix([1,1],
                                                         [-30,30],
@@ -68,45 +68,46 @@ def eval_model(
         nifti_image = nib.Nifti1Image(TestImage[0,...], mov_affine)
         nib.save(nifti_image, './rotated'+str(i))
         
+        tmp = np.moveaxis(TestImage, i, 1)
+        
         GT_rot_mat = create_rotation_matrix(rotation_mov)
         vec5_GT = vrrotmat2vec(GT_rot_mat)
         vec3_GT = vec5_to_vec3(vec5_GT)
         inv_GT_rot_mat = np.linalg.inv(GT_rot_mat)
 
-        vec3_pred = model.predict(TestImage[...,60:61], batch_size=1)
+        vec3_pred = model.predict(tmp[...,55:56], batch_size=1)
         vec5_pred = vec3_to_vec5(vec3_pred[0, :])
         pred_rot_mat = vrrotvec2mat(vec5_pred)
         inv_pred_rot_mat = np.linalg.inv(pred_rot_mat)
         
-        affine_mov, rotation_mov = create_affine_matrix([1,1],
-                                                        inv_GT_rot_mat,
-                                                        [0,0],
-                                                        target_size)
+#         affine_mov_GT, rotation_mov = create_affine_matrix([1,1],
+#                                                         inv_GT_rot_mat,
+#                                                         [0,0],
+#                                                         target_size)
 
-        GT_rot_back_img, _ = similarity_transform_volumes(TestImage[0,:,:,:],
-                                                            affine_mov,
-                                                            target_size,)
-        nifti_image = nib.Nifti1Image(GT_rot_back_img, mov_affine)
-        nib.save(nifti_image, './GT_Backed'+str(i))
+#         GT_rot_back_img, _ = similarity_transform_volumes(TestImage[0,:,:,:],
+#                                                             affine_mov_GT,
+#                                                             target_size,)
+#         nifti_image = nib.Nifti1Image(GT_rot_back_img, mov_affine)
+#         nib.save(nifti_image, './GT_Backed'+str(i))
         
-        affine_mov, rotation_mov = create_affine_matrix([1,1],
+        affine_mov_pred, rotation_mov = create_affine_matrix([1,1],
                                                         inv_pred_rot_mat,
                                                         [0,0],
                                                         target_size)
 
         pred_rot_back_img, _ = similarity_transform_volumes(TestImage[0,:,:,:],
-                                                            affine_mov,
+                                                            affine_mov_pred,
                                                             target_size,)
         nifti_image = nib.Nifti1Image(pred_rot_back_img, mov_affine)
         nib.save(nifti_image, './Pred_Backed'+str(i))
         
-        distance = geodesic_distance(GT_rot_mat, pred_rot_mat)
-#         result[i,:-1] = rotation_mov
-        result[i,-1] = distance
+        Rot_distance2 = rot_distance(GT_rot_mat, pred_rot_mat)*180/3.1415
+
         print("True rotation vector is: {}".format(vec3_GT))
         print("Predicted rotation vector is: {}".format(vec3_pred[0, :]))
         print("MSE of rotation vectors is: {}".format(np.linalg.norm((vec3_pred[0, :]- vec3_GT))))
-        print("Geodesic distance is: {}".format(distance))
+        print("Rot distance is: {}".format(Rot_distance2))
         print('---------')
     return
 

train_2d.py
@@ -1,4 +1,6 @@
 import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 from datetime import datetime, timedelta
 
 import click
@@ -16,7 +18,7 @@ from .util import (
     vec3_to_vec5,
     vrrotvec2mat,
     geodesic_distance,
-    
+    rot_distance,
 )
 from losses import geodesic
 # -----------------------------------------------------------------------------
@@ -35,29 +37,36 @@ def train_model():
     sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='mean_squared_error', optimizer=sgd)
     current_time = datetime.now() + timedelta(hours=-5)
-    tbCallBack = keras.callbacks.TensorBoard(log_dir='/host/home/exx/Documents/tensorboard/'+str(current_time)[:19], histogram_freq=0, write_graph=True, write_images=False)
+    tbCallBack = keras.callbacks.TensorBoard(log_dir='/host/home/exx/Documents/tensorboard/'+str(current_time)[:19], histogram_freq=0, write_graph=True, write_images=True)
 
     for j in range(30):
         for i in range(65):
             images = np.load('/host/data/30DegreeData/imageData'+str(i)+'.npy')
             labels = np.load('/host/data/30DegreeData/Label'+str(i)+'.npy')
             print('data loaded')
-            
-            images = images[:,:,:,50:80]
-            images = np.moveaxis(images, -1, 1)
-            images = np.reshape(images, (-1 ,width, height, n_channels))
-            
+
+            img2feed = np.zeros((900, width, height, 30))
+            for dim in range(1, 4):
+                random_slices = np.random.randint(50, 80, 10)
+                tmp = np.moveaxis(images, dim, 1)
+                tmp = tmp[..., random_slices]
+                img2feed[..., (dim-1)*10:dim*10] = tmp
+
+            img2feed = np.moveaxis(img2feed, -1, 1)
+            img2feed = np.reshape(img2feed, (-1 ,width, height, n_channels))
             labels = np.reshape(np.tile(labels,30), (-1, 3))
+            del images
+            print('data rearranged')
             
-            model.fit(images[:-(75*30)],
+            model.fit(img2feed[:-(75*30)],
                       labels[:-(75*30)],
                       batch_size=32,
                       epochs=(65*10*j)+10*i+10,
                       initial_epoch = (65*10*j)+10*i,
                       callbacks=[tbCallBack],
-                      validation_data=(images[-(75*30):], labels[-(75*30):]),
+                      validation_data=(img2feed[-(75*30):], labels[-(75*30):]),
                       shuffle=True)
-            model.save('/host/home/exx/Documents/resources/model/2dTo3d_3.h5')
+            model.save('/host/home/exx/Documents/resources/model/3view_2dTo3d_4.h5')
     
     return
 

util.py
@@ -2,6 +2,7 @@ import nibabel as nib
 import nilearn.image as nil_image
 import numpy as np
 import SimpleITK as sitk
+import scipy
 
 def load_3d_data(
     filename,
@@ -395,14 +396,35 @@ def geodesic_distance(
             np.clip(
                 0.5*np.trace(
                     np.matmul(
-                        np.transpose(rot1),
                         rot2,
+                        np.linalg.inv(rot1),
                     )
                 )-1,
                 -1.0+1e-7,
                 1.0-1e-7,
             )
         )
+    )
+
+    return distance
+
+
+def rot_distance(
+    rot2,
+    rot1,
+):
+    distance = (1/np.sqrt(2)) * np.linalg.norm(
+        scipy.linalg.logm(
+            np.clip(
+                np.matmul(
+                   rot2,
+                   np.transpose(rot1),
+                ),
+                0,
+                2,
+            )
+        ),
+        ord = 'fro',
     )
 
     return distance